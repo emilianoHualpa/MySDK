b1f3820b51b0d08e9b80d1d6ec061a0cdf025015
Emiliano Hualpa <emiliano.hualpa2@altran.com>
2019-02-01 10:44:28 +0100

    Initial commit for SDK

---
+++ MySDK/MiLoG.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  MySDK
+//
+//  Created by Emiliano Hualpa on 2/1/19.
+//  Copyright © 2019 Milo Development. All rights reserved.
+//
+
+import Foundation
+
+public class MiLoG {
+    public static func milog<T>(_ text: T) {
+        print("MiLoG::: \(text)")
+    }
+}
